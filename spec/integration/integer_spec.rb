9bc413dc3951a459470129ffc5d516e4eb68529c
Sergio Bobillier <sergio.bobillier@gmail.com>
2021-01-19 23:13:39 +0100

    Create integration tests for the Integer class
    Tests for the Integer -> Array -> Integer conversion.

---
+++ spec/integration/integer_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require_relative '../../refinaments/integer'
+require_relative '../../refinaments/array'
+
+RSpec.describe Integer do
+  describe '#to_a -> #to_i' do
+    context 'with an 8-bit positive integer' do
+      subject(:integer) { rand(0..255) }
+
+      it 'returns the expected integer' do
+        expect(integer.to_a.to_i).to eq(integer)
+      end
+    end
+
+    context 'with an 8-bit negative integer' do
+      subject(:integer) { rand(-128...0) }
+
+      let(:expected_value) { integer + 256 }
+
+      it "returns the Integer's two's complement" do
+        expect(integer.to_a.to_i).to eq(expected_value)
+      end
+    end
+
+    context 'with a 16-bit positive integer' do
+      subject(:integer) { rand(256..65535) }
+
+      it 'returns the expected integer' do
+        expect(integer.to_a.to_i).to eq(integer)
+      end
+    end
+
+    context 'with a 16-bit negative integer' do
+      subject(:integer) { rand(-32768...0) }
+
+      let(:expected_value) { integer + 65536 }
+
+      it 'returns the expected integer' do
+        expect(integer.to_a.to_i).to eq(expected_value)
+      end
+    end
+  end
+end
